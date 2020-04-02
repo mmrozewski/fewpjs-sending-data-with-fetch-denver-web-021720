b59f8c4aee4bbbe5bd44fd69546bcfc22a22e624
mmrozewski <mmrozews@gmail.com>
2020-04-02 17:50:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+
+function submitData(username, email) {
+    configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: username,
+            email: email
+        })
+    };
+    return fetch("http://localhost:3000/users", configurationObject)
+            .then(function(response) {return response.json()})
+            .then(function(object) {document.body.innerHTML = object["id"]})
+            .catch(function(error) {document.body.innerHTML = error.message})
+
+}
+
+
+
+
